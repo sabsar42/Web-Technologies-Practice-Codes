45dcff6057d008c391aadadb90ca9b77ecd58dc0
Shakib Absar <sabsar42@gmail.com>
2024-05-15 23:43:57 +0600

    updates

---
+++ form/formvalidation.js
@@ -0,0 +1,62 @@
+function form(){
+    var fname= document.getElementById("ifname").value;
+    var email= document.getElementById("iemail").value;
+    var mobile= document.getElementById("imob").value;
+    var pass= document.getElementById("ipass").value;
+    var namepattern= /[a-zA-Z.]/;
+    var emailpattern=/(cse|eee|law)_\d{10}@lus\.ac\.bd/;
+    var mobpattern=/(\+88)?-?01[3-9]\d{8}/;
+    var passpattern= /((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*><?()*&+_])).{8,20}/;
+
+    //first name
+    if(fname.length<2 || fname.length>20){
+        // alert("length must be 2-20");
+        document.getElementById("efname").innerHTML="length must be 2-20";
+        return false;
+    }
+    else if(!fname.match(namepattern)){
+        // alert("invalid input");
+        document.getElementById("efname").innerHTML="invalid input";
+        return false;
+    }
+    else{
+        // alert("valid");
+        document.getElementById("efname").innerHTML="";
+        
+    }
+//email
+if(!email.match(emailpattern)){
+    // alert("invalid input");
+    document.getElementById("eemail").innerHTML="invalid input";
+    return false;
+}
+else{
+    // alert("valid");
+    document.getElementById("eemail").innerHTML="";
+    
+}
+//mobile
+if(!mobile.match(mobpattern)){
+    // alert("invalid input");
+    document.getElementById("emob").innerHTML="invalid input";
+    return false;
+}
+else{
+    // alert("valid");
+    document.getElementById("emob").innerHTML="";
+    
+}
+
+//password
+if(!pass.match(passpattern)){
+    // alert("invalid input");
+    document.getElementById("epass").innerHTML="invalid input";
+    return false;
+}
+else{
+    // alert("valid");
+    document.getElementById("epass").innerHTML="";
+    
+}
+
+}
---
+++ ls
@@ -0,0 +1,30 @@
+[33mcommit 7d09bb9237ee838a82fa933aa138e705d85a29d2[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Shakib Absar <sabsar42@gmail.com>
+Date:   Wed May 15 23:13:41 2024 +0600
+
+    Web Code Updated
+
+[33mcommit 66feaf78184dbc6978ae4a3a659170374f9724d2[m
+Merge: 505fce2 d17511a
+Author: Shakib Absar <sabsar42@gmail.com>
+Date:   Fri Mar 1 21:23:06 2024 +0600
+
+    Merge branch 'main' of https://github.com/sabsar42/Web-Technologies-Practice-Codes
+
+[33mcommit 505fce223787f93f53fffa9650ad23c2a1b7c354[m
+Author: Shakib Absar <sabsar42@gmail.com>
+Date:   Fri Mar 1 21:22:49 2024 +0600
+
+    Updated Code
+
+[33mcommit d17511adbd78b0206854f86a163334413cfc36d1[m
+Author: Shakib Absar <sabsar42@gmail.com>
+Date:   Tue Jan 30 02:12:26 2024 +0600
+
+    Create README.md
+
+[33mcommit 4c97fc3eda5050c8723ffad35a886726c14abdfc[m
+Author: Shakib Absar <sabsar42@gmail.com>
+Date:   Tue Jan 30 02:11:30 2024 +0600
+
+    first commit
